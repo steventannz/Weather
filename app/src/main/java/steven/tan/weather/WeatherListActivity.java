b337362fbe9bb54c8fdd8f52cbd55675c3803d1d
Steven Tan <steven.tan@hotmail.co.nz>
2017-07-26 17:25:04 +1200

    Initial commit

---
+++ app/src/main/java/steven/tan/weather/WeatherListActivity.java
@@ -0,0 +1,17 @@
+package steven.tan.weather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class WeatherListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
---
+++ app/src/main/java/steven/tan/weather/WeatherListActivityFragment.java
@@ -0,0 +1,22 @@
+package steven.tan.weather;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class WeatherListActivityFragment extends Fragment {
+
+    public WeatherListActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather_list, container, false);
+    }
+}
